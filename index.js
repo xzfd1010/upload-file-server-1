7251b79017e08ff60f86294b5105e88874855eeb
Nick <zhongcheng509@gmail.com>
2020-02-28 15:52:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const multer = require('multer')
+const cors = require('cors')
+const upload = multer({ dest: 'uploads/' })
+
+const app = express()
+
+app.options('/upload', cors())
+app.post('/upload', cors(), upload.single('file'), (req, res) => {
+  res.send(req.file.filename)
+})
+
+app.get('/preview/:key', cors(), (req, res) => {
+  res.sendFile(`uploads/${req.params.key}`, {
+    root: __dirname,
+    headers: {
+      'Content-Type': 'image/jpeg',
+    },
+  }, (error) => {
+    if (error) {
+      res.status(404).send('file not found')
+    }
+  })
+})
+app.listen(3000)
